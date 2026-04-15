923e78acd938430d99e0c58a2a891091a47b7710
Lautaro Gonzalez <gonzalezlautaro03@gmail.com>
2026-04-15 08:31:20 -0300

    fix final router + deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AppRoutes } from './router';
 import { CartProvider } from './contexts/CartContext';
 import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
-    <BrowserRouter basename={__BASE_PATH__}>
+    <HashRouter>
       <ScrollToTop />
       <CartProvider>
         <AppRoutes />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
